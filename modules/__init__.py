02805a0fa0370aef05907d3a66434e1884d435db
caoTayTang <ledai3062005@gmail.com>
2025-09-11 19:32:08 +0700

    feat: Fix modules

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,14 @@
 from .eda_fn import *
+from .configurator import *
+
+import ipywidgets as widgets
+from IPython.display import display, clear_output
+from itertools import product
+
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import xgboost as xgb
+
--- modules/configurator.py
+++ modules/configurator.py
@@ -6,10 +6,6 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import xgboost as xgb
 
